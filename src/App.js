486d0722d7fc67b38f599382fbd1cc3684b7b7cf
LikozGO <likoz98@gmail.com>
2020-04-12 18:19:30 +0300

    folder commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.scss';
+import Navigation from "./PageAdmin/Navigation/Navigation";
 
 function App() {
     return (
-        <div className="section-1 container">
-            <div className="row text-center">
-                <div className="col-md-6 col-12">Hello World1</div>
-                <div className="col-md-6 col-12">Hello World2</div>
-            </div>
-        </div>
+        <Navigation/>
     );
 }
 
---
+++ src/PageAdmin/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+function Navigation() {
+    return (
+        <>
+        </>
+    );
+}
+
+export default Navigation;
