f6b6b7d2abe6819c181af86b73b5bdd1e99082f2
LikozGO <likoz98@gmail.com>
2020-07-19 20:53:41 +0300

    env replace

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = process.env.ATLAS_URI;
+const uri = 'mongodb+srv://admin:4128902@likoz-lyyzf.gcp.mongodb.net/test?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
 const { connection } = mongoose;
 connection.once('open', () => {
