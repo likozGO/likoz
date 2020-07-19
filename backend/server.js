e48b249b6d02026c96a50104d5804bc09766420f
LikozGO <likoz98@gmail.com>
2020-07-19 21:48:25 +0300

    fix path

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ connection.once('open', () => {
 const productsRouter = require('./routes/products');
 const usersRouter = require('./routes/users');
 
-app.use('api/products', productsRouter);
-app.use('api/users', usersRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/users', usersRouter);
 
 app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', (req, res) => {
